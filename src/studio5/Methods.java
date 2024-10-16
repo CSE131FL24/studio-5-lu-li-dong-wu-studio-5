c779fc72ba833e1ec17bb0379e74d379f8326448
wyqr3 <wyqr3@localhost>
2024-10-16 13:58:58 -0500

    finished

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,5 +1,7 @@
 package studio5;
 
+import java.awt.Color;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
@@ -14,7 +16,7 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -34,19 +36,27 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		Color blue = new Color (0,109,219);
+		StdDraw.setPenColor(blue);
+		StdDraw.filledCircle(x, y, radius * 3/4);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		Color red = new Color (146,0,0);
+		StdDraw.setPenColor(red);
+		StdDraw.filledCircle(x, y, radius * 1/2);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		Color yellow = new Color (255,255,109);
+		StdDraw.setPenColor(yellow);
+		StdDraw.filledCircle(x, y, radius * 1/4);
 		
 	}
+	
+	
 
 	/**
 	 * Return a new String which is the original source String with all occurrences
@@ -75,7 +85,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
 		return sum;
 	}
 
@@ -87,8 +99,11 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
+		for (int i = 0; i < length; i++) {
+			values[i] = value;
+		}
 
 		
 
@@ -96,7 +111,18 @@ public class Methods {
 	}
 
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
+	public static double arrayMean(int[]values) {
+		int sum = 0;
+		for (int i = 0; i < values.length; i++) {
+			sum+=values[i];
+		}
+		return (double)(sum)/values.length ;
+		
+	}
 	// TODO: Create a JavaDoc comment for the arrayMean method.
-
+    /**calculating array mean
+     * ...
+     */
+	
 	
 }
